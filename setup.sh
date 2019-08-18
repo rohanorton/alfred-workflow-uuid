1bd80913ad29999efc8e0f523da19ff3d64f57e0
Rohan Orton <rohan.orton@gmail.com>
2019-08-18 19:57:23 +0100

    add icon

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ export SCRIPT_CONTENT=$(cat main.py)
 
 mkdir -p ${ALFRED_DIR}/user.workflow.${ID}
 cat info.plist.tpl.xml | envsubst >  ${ALFRED_DIR}/user.workflow.${ID}/info.plist
+cp -rf icon.png ${ALFRED_DIR}/user.workflow.${ID}/icon.png
